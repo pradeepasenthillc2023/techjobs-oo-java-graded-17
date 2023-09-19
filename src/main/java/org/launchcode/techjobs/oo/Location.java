f19fc78bbf8b47d681b4f1d800a0705bbbfcaa1f
PRADEEPA SENTHILRAJAN <pradeepa.senthil@gmail.com>
2023-09-19 19:02:48 -0400

    Initial commit

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -16,8 +16,15 @@ public class Location {
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
+    public Location(String value) {
+        this.value = value;
+    }
 
-    // Custom toString, equals, and hashCode methods:
+    public Location(int id) {
+        this();
+        this.id = id;
+    }
+// Custom toString, equals, and hashCode methods:
 
     @Override
     public String toString() {
